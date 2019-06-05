497910b19acf553e61103214bd2eb60f753956c7
ounenui-wnw <36441189+111ANY@users.noreply.github.com>
2019-06-05 11:55:13 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php 
+// // 設定當前目錄為應用目錄
+// define('APP_P', __DIR__.'/');
+// // 調試模式設定
+// define('DEBUG_NODE', true);
+// 網站根網址
+define('APP_URL', 'http://moca.oniany.xin/');
+// 加載框架
+require './MOCA/MOCA.php';
